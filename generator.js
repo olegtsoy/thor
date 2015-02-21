a1e658dfb0f1b246d87c6e6d792557b913c713d7
oleg <oleg_ts@live.ru>
2015-02-21 22:49:11 +0300

    Some report view changes changes

--- generator.js
+++ generator.js
@@ -12,11 +12,20 @@ exports.utf8 = function utf(size, fn) {
   var key = 'utf8::'+ size
     , cached = cache[key];
 
+  // Start custom msg
+  var msg = '{"commandName":"Connect","commandUniqueId":"12345","userData":{"okId":777,"firstName":"Oleg","lastName":"Tsoy","gender":"m","birthday":"1992-20-05","age":22,"location":"Piter","nickName":"Alligator:2000","accountBill":1000,"avatarUrl":"123","registrationId":"APA91bGnsHLfJQY8ffpj6wfacg4bfIjDE6QhueORRX3UnhN_b4pUEiXAKLPwBYkAGcPst3vEwYb25NWquv2azBCDZ5rjIHFt1cGLuRU2g68VOLK0Rgrj4c-2WlTZCCKxuqZDaynM6sBV_TcXDBTc8_GrOzqFfZhNOGUQNibFBQot_87eQBEKIPc"}}';
+  // End
   // We have a cached version of this size, return that instead.
-  if (cached) return fn(undefined, cached);
+  // Start
+  if (cached) return fn(undefined, msg);
+  //if (cached) return fn(undefined, cached);
+  // End
 
   cached = cache[key] = new Buffer(size).toString('utf-8');
+  // Start
   fn(undefined, cached);
+  //fn(undefined, cached);
+  // End
 };
 
 /**
